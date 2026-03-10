2f0db7688bc1af0015d5cc07cbfeafd4ddaa7790
AavanVadiwala <aavanv@hotmail.com>
2026-03-10 16:47:09 -0400

    Create proj1av.c
    its not completed but just progress save

---
+++ proj1av.c
@@ -0,0 +1,130 @@
+/*
+Operating Systems Project part 1
+
+Aavan Vadiwala and Isabel Gilchrest
+
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
+#include <stdbool.h>
+#include <ctype.h>
+#include <stddef.h>
+#include <sys/wait.h>
+#include <sys/resource.h>
+
+/*For each of the n processes, in order A0 through Z9, perform the steps below, with I/O-bound
+processes generated first. Note that all generated values are integers, even though we use a floatingpoint pseudo-random number generator.
+Define your exponential distribution pseudo-random number generation function as next_exp()
+(or another similar name) and have it return the next pseudo-random number in the sequence,
+capped by *(argv+5).
+*/
+
+void createPIDs( int n, char ** processIDs){
+	int count = 0;
+	char letter = 'A';
+	for( int i = 0; i < n; i++){
+		if ( count >= 10){
+			letter++;
+		}
+		else{ 
+			count++; 
+		}
+		fprintf(*(processIDs + i), "%s%d", letter, count);
+	}
+}
+
+void createPIDs(int n, char **processIDs) {
+    char letter = 'A';
+    for (int i = 0; i < n; i++) {
+        processIDs[i] = malloc(4); // Space for 'A0\0'
+        sprintf(processIDs[i], "%c%d", letter + (i / 10), i % 10);
+    }
+}
+
+/*2. Identify the number of CPU bursts for the given process as the “ceiling” of the next random
+number generated from the uniform distribution obtained via drand48(), then multiplied
+by 16; this should obtain a random integer in the inclusive range [1, 16]*/        
+double next_exp(double lamda, int upperBound) {
+    double val = upperBound;
+    while (val > upperBound) {
+        double r = drand48();
+        val = -log(r) / lamda;
+    }
+    return val;
+}
+
+void getNumBurstsarrivals(double lamda, int upperBound, int* arrival, int* num_bursts) {
+    *arrival = (int)floor(next_exp(lamda, upperBound));
+    *num_bursts = (int)ceil(drand48() * 16); // Use drand48() for uniform dist
+}
+
+/*3. For each of CPU burst in turn, identify the CPU burst time and the I/O burst time as
+the “ceiling” of the next two pseudo-random numbers given by next_exp(); for I/O-bound
+processes, multiply the I/O burst time by 8 such that I/O burst time is close to an order of
+magnitude longer than CPU burst time; as noted above, for CPU-bound processes, multiply
+the CPU burst time by 6; and for the last CPU burst, do not generate a corresponding I/O
+burst time, since each process ends with one final CPU burst*/
+void getBursts(float *CPU_burst_times, float *IO_burst_times, double lamda, int upperBound, int num_bursts) {
+    for (int i = 0; i < num_bursts - 1; i++) {
+        CPU_burst_times[i] = ceil(next_exp(lamda, upperBound)) * 6;
+        IO_burst_times[i] = ceil(next_exp(lamda, upperBound)) * 8; 
+    }
+    CPU_burst_times[num_bursts - 1] = ceil(next_exp(lamda, upperBound)) * 6;
+}
+
+int main( int argc, char ** argv )
+{
+	
+	setvbuf(stdout, NULL, _IONBF, 0);
+
+  	if ( argc != 6 ) {
+    	fprintf( stderr, "ERROR: Invalid arguments\n" );
+    	fprintf( stderr, "USAGE: proj1.out <n> <ncpu> <seed> <lambda>  <threshold>\n" );
+    	// return EXIT_FAILURE;
+    	abort();
+  	}
+
+	// implemnet eror checking for each argv (if not null or wrong type)
+
+    int n = atoi(argv[1]);
+    int ncpu = atoi(argv[2]);
+    int seed = atoi(argv[3]);
+    double lamda = atof(argv[4]);
+    int upperBound = atoi(argv[5]);
+    srand48(seed);
+
+	char ** processIDs = calloc( n, sizeof(char*));
+	createPIDs( n, &processIDs);
+
+    for( int i = 0; i < n; i++){
+            
+        printf( "%s", *(processIDs + i) );
+        int arrival, num_bursts;
+        getNumBurstsarrivals(lamda, upperBound, &arrival, &num_bursts);
+
+        float *CPU_burst_times = malloc(num_bursts * sizeof(float));
+        float *IO_burst_times = malloc((num_bursts - 1) * sizeof(float));
+
+        // Pass the pointers directly
+        getBursts(CPU_burst_times, IO_burst_times, lamda, upperBound, num_bursts);
+
+        // Print results here...
+
+        free(CPU_burst_times);
+        free(IO_burst_times);
+        free(processIDs);
+        
+
+    }
+
+
+	return EXIT_SUCCESS;
+}
+
+
+
+
